8061a5183a48ccf5108ddd18db32ef2f401c174f
Yatin Sarbalia <yatinsns@gmail.com>
2015-09-26 11:42:26 +0530

    Initial Commit

---
+++ TestReachability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestReachability
+//
+//  Created by Yatin on 9/26/15.
+//  Copyright (c) 2015 Yatin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestReachability/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestReachability
+//
+//  Created by Yatin on 9/26/15.
+//  Copyright (c) 2015 Yatin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
