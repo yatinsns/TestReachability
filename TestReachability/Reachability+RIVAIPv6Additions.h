40f12990cbef3dc7dcc1159d9c98f38e262bc5ce
Yatin Sarbalia <yatinsns@gmail.com>
2015-09-30 12:09:14 +0530

    Fix reachability

--- TestReachability/Reachability+RIVAIPv6Additions.h
+++ TestReachability/Reachability+RIVAIPv6Additions.h
@@ -4,6 +4,12 @@
 
 /**
  Creates and return a new `Reachability` object initialized to track reachability
+ in IPv4 only network.
+ */
++ (Reachability *)riva_reachabilityForInternetConnectionIPv4Only;
+
+/**
+ Creates and return a new `Reachability` object initialized to track reachability
  in IPv6 only network.
  */
 + (Reachability *)riva_reachabilityForInternetConnectionIPv6Only;
--- TestReachability/Reachability+RIVAIPv6Additions.m
+++ TestReachability/Reachability+RIVAIPv6Additions.m
@@ -5,15 +5,28 @@
 
 @implementation Reachability (RIVAIPv6Additions)
 
++ (Reachability *)riva_reachabilityForInternetConnectionIPv4Only {
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(struct sockaddr_in));
+  addr.sin_len = sizeof(struct sockaddr_in);
+  addr.sin_family = AF_INET;
+
+  return [self riva_reachabilityWithAddress:&addr];
+}
+
 + (Reachability *)riva_reachabilityForInternetConnectionIPv6Only {
   struct sockaddr_in6 addr;
   memset(&addr, 0, sizeof(struct sockaddr_in6));
-  (&addr)->sin6_len = sizeof(struct sockaddr_in6);
-  (&addr)->sin6_family = AF_INET6;
+  addr.sin6_len = sizeof(struct sockaddr_in6);
+  addr.sin6_family = AF_INET6;
   addr.sin6_addr = in6addr_any;
   
+  return [self riva_reachabilityWithAddress:&addr];
+}
+
++ (Reachability *)riva_reachabilityWithAddress:(void *)hostAddress {
   SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault,
-                                                                        (const struct sockaddr*)&addr);
+                                                                        (const struct sockaddr*)hostAddress);
   if (ref != nil) {
     Reachability *reachability = [[self alloc] initWithReachabilityRef:ref];
     return reachability;
