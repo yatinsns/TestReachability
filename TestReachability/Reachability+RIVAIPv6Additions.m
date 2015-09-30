185092cbc06afdcac598aef51c2ab0264977d515
Yatin Sarbalia <yatinsns@gmail.com>
2015-09-30 11:52:09 +0530

    Fix reachability category

--- TestReachability/Reachability+RIVAIPv6Additions.m
+++ TestReachability/Reachability+RIVAIPv6Additions.m
@@ -6,15 +6,11 @@
 @implementation Reachability (RIVAIPv6Additions)
 
 + (Reachability *)riva_reachabilityForInternetConnectionIPv6Only {
-  char strAddr[INET6_ADDRSTRLEN];
-  
   struct sockaddr_in6 addr;
   memset(&addr, 0, sizeof(struct sockaddr_in6));
   (&addr)->sin6_len = sizeof(struct sockaddr_in6);
   (&addr)->sin6_family = AF_INET6;
   addr.sin6_addr = in6addr_any;
-  inet_ntop(AF_INET6, &addr.sin6_addr, strAddr, INET6_ADDRSTRLEN);
-  inet_pton(AF_INET6, strAddr, &addr.sin6_addr);
   
   SCNetworkReachabilityRef ref = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault,
                                                                         (const struct sockaddr*)&addr);
